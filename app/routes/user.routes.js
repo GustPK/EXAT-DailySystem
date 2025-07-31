fe07adcfdc7f2836b434d9b426dba473a697555a
GustPK <pasawat.k@ku.th>
2025-07-31 14:51:47 +0700

    update

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -7,7 +7,7 @@ const authenBasic = require('../middlewares/authentication.middleware')
 router.post("/v1/user/signup", user.signup)
 router.post("/v1/user/login", user.login);
 router.put("/v1/user/update", authenBasic, user.update);
-router.post("/v1/user/get", user.get)
+router.post("/v1/user/get", authenBasic, user.get);
 
 
 module.exports = router
